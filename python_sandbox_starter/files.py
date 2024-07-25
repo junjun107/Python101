45c36fbea2580361944706defcc7bd6843b2f20e
June Zhao <bz.bijun@gmail.com>
2024-07-24 20:20:11 -0700

    files and json

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,16 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+
+#open a file 
+myFile = open('myfile.txt','w')
+
+#get info of file
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+
+#write to file
+myFile.write("I love Python")
+myFile.write(' and JavaScript')
+myFile.close()
--- python_sandbox_starter/py_json.py
+++ python_sandbox_starter/py_json.py
@@ -1,1 +1,18 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+
+import json
+
+userJson = '{"first_name": "John", "last_name": "Smith", "age": 30}'
+
+#Parse JSON to Dictionary
+user = json.loads(userJson)
+
+# print(user)
+# print(user['first_name'])
+
+#Turn Dictionary into JSON
+car = {'make': 'Ford', 'model': 'Mustang', 'year':1970}
+
+carJSON = json.dumps(car)
+
+print(carJSON)

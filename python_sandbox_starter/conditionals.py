6c57488c08eaeef97461011aad2130696ffcd79e
June Zhao <bz.bijun@gmail.com>
2024-09-12 20:40:42 -0700

    python exercise

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -1,51 +1,20 @@
-# If/ Else conditions are used to decide to do something based on something being true or false
-# x= 10
-# y = 10
+def minimal_number_of_packages(items, available_large_packages, available_small_packages):
+ #Fill 3 large boxes itmes /15 
+# check remainder, items - items/15
+  remaining_items = items
+  large_capacity = 5
+  small_capacity = 1
+  large_box_needed = items// large_capacity
+  remaining_items = items% large_capacity
 
-
-# Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
-
-#if 
-# if x > y:
-#   print(f'{x} is greater than {y}')
-
-# Logical operators (and, or, not) - Used to combine conditional statements
-
-#if/else
-# if x>y:
-#   print(f'{x} is greater than {y}')
-# else:
-#   print(f'{y} is greater than {x}')
-
-#elif
-## if -> else if -> else 
-# if x>y:
-#   print(f'{x} is greater than {y}')
-# elif x<y:
-#   print(f'{y} is greater than {x}')
-# else:
-#   print("two numbers are the same")
-
-  
-# Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
-
-
-
-# Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
- # and 
- # or 
-# not 
-
-height = int(input("what's your height in cm?"))
-if height >= 120:
-  
-  print("You can get on the ride")
-  age = int(input("how old are you?"))
-  if age > 18:
-    print("Please pay $12")
-  elif age >12 and age < 18:
-    print("Please pay 7") 
+  if remaining_items> available_large_packages:
+     large_box_needed+= 1
+     remaining_items -= available_small_packages
+     available_small_packages = 0
   else:
-    print("Please pay $5") 
-else:
-  print("Sorry you cannot get on the ride")
+     available_small_packages -= remaining_items
+
+if __name__ == "__main__":
+    print(minimal_number_of_packages(13, 3, 10))
+
+  

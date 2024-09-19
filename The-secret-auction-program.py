5ec900d3bb2aff66f94b087c3c102f0cb734bcef
June Zhao <bz.bijun@gmail.com>
2024-09-18 22:30:08 -0700

    auction

--- The-secret-auction-program.py
+++ The-secret-auction-program.py
@@ -11,37 +11,35 @@
 # should_continue = input("Adding more bids? y or n")
 
 #loop over dict
-# Todo4: Compare for highests
+# Todo4: find out highests bids
 
-import art
+#save data into a dictionary
+#anymore bidders?
+#use a while loop to continute bidding
 
-def find_highest_bidder(bidding_dict):
-  winner = ""
+def find_highest_bidder(bidding_dict): #function to find highest bidder, loop over object
   highest_bid = 0
-
-  max(bidding_dict)
-
+  winner = ""
   for bidder in bidding_dict:
     bid_amount = bidding_dict[bidder]
     if bid_amount > highest_bid:
-      highest_bid= bid_amount
+      highest_bid = bid_amount #find highest bid
       winner = bidder
-  print(f"The winnder is ${winner} with a bid of ${highest_bid}")
-bids= {}
+  print(f"Winner is {winner} with bid: ${highest_bid}")
+
+bids = {}
 
 continue_bidding = True
 
 while continue_bidding:
-  name = input("What's your name?: ")
+  name = input("Enter your name:")
   price = int(input("Enter your bid: "))
   bids[name] = price
-  should_continue = input("Adding more bids? y or n").lower()
-  if should_continue == 'n':
+  should_continue = input("Are there any more bidders? y or n").lower()
+  if should_continue == "n": #find highest bid 
     continue_bidding = False
     find_highest_bidder(bids)
   elif should_continue == "y":
-    print("\n" * 20)
-
-
-print(art.logo)
+    print("\n"* 20 )#clear screen so no one sess previous bid
+    #loop back
 

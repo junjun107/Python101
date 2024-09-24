7e1a53b17fac56a6c7d8568a463a3345e2743aea
June Zhao <bz.bijun@gmail.com>
2024-09-23 20:55:19 -0700

    recursion

--- python_sandbox_starter/day_9_calculator.py
+++ python_sandbox_starter/day_9_calculator.py
@@ -1,54 +1,48 @@
-#write out all four math functions
-# add four math functions into a dictionary as values, keys = "+","-","*","/"
-# try multiply 4 * 8
-
-# use recursion to solve this problem, also works with another while loop
-
 def add(n1, n2):
-  return n1 + n2
+    return n1 + n2
 
 def subtract(n1, n2):
-  return n1 - n2
+    return n1 - n2
 
 def multiply(n1, n2):
-  return n1 * n2
+    return n1 * n2
 
 def divide(n1, n2):
-  return n1 / n2
-
-
-calculator_dict = {
-  "+" : add,
-  "-" : subtract,
-  "*" : multiply,
-  "/" : divide,
-}
+    return n1 / n2
 
 def calculator():
-  should_accumulate = True
-
-  num1 = float(input("what is your 1st number?: "))
-
-  while should_accumulate:
-
-    for symbol in calculator_dict:
-      print(symbol)
-
-    math_operator = input("what is the operation?: " )
-
-    num2 = float(input("what is your next number?: "))
-
-    answer = calculator_dict[math_operator](num1, num2)
-
-    print(f"{num1} {math_operator} {num2} = {answer}")
-
-    choice = input(f"Type 'y' to continue with {answer}, or type 'n' to start a new calculation ")
-
-    if choice == 'y' :
-      num1 = answer
-    else:
-      should_accumulate = False
-      print("\n" *20)
-      calculator()
+    operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+    
+    should_accumulate = True
+    
+    n1 = float(input("Enter first number: "))
+    
+    while should_accumulate:
+        for key in operations:
+            print(key)
+    
+        choice = input("Enter a math operation from above: ")
+        n2 = float(input("Enter second number: "))
+    
+        result = operations[choice](n1, n2)
+        print(f"{n1} {choice} {n2} is {result}")
+    
+        # Ask if user wants to continue working with previous number
+        should_continue = input(
+            f"Enter 'y' to continue wokring with previous number {result} or enter 'n' to start a new calculation "
+        )
+    
+        if should_continue == "y":
+            n1 = result
+        else: 
+            should_accumulate = False
+            print("\n" *40)
+            calculator()
 
 calculator()
+# if continue, get user symbol choice, do math. basicallly start all over again. sooooooo use a while loop
+# start a variable called should_accumulate, place at top
+# if user don't want to continue, set should_accumulate to False
+# start over again : method 1: put everything inside a while loop
+# method 2: put everything inside a function, call the function inside itself -> the else part. RECURSION
+

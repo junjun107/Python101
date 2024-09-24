e01f89c80da76a200a700a267efecd1deefa550e
June Zhao <bz.bijun@gmail.com>
2024-09-23 21:16:37 -0700

    balckjack initial pseudocode

---
+++ python_sandbox_starter/black_jack.py
@@ -0,0 +1,24 @@
+# blackjackValues = {
+#     '2': 2,
+#     '3': 3,
+#     '4': 4,
+#     '5': 5,
+#     '6': 6,
+#     '7': 7,
+#     '8': 8,
+#     '9': 9,
+#     '10': 10,
+#     'J': 10,
+#     'Q': 10,
+#     'K': 10,
+#     'A': [1, 11] 
+# }
+
+cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+
+# start, y or n
+# your cards are [num1, numn2], sum 
+# computer's cards are [num1, num2], sum
+
+# do you want anohter card? 
+  # if y, add num3, sum 

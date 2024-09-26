c5b5b70c1e0383e70e2e80af711b543f61ab189e
June Zhao <bz.bijun@gmail.com>
2024-09-25 21:10:14 -0700

    black_jack initial setup

--- python_sandbox_starter/black_jack.py
+++ python_sandbox_starter/black_jack.py
@@ -1,24 +1,26 @@
-# blackjackValues = {
-#     '2': 2,
-#     '3': 3,
-#     '4': 4,
-#     '5': 5,
-#     '6': 6,
-#     '7': 7,
-#     '8': 8,
-#     '9': 9,
-#     '10': 10,
-#     'J': 10,
-#     'Q': 10,
-#     'K': 10,
-#     'A': [1, 11] 
-# }
+# player get two number from list
+    # sum the two , as long as the sum is < 21 keep asking if player wants another card
+# computer get one number from list
+    # if yes, add another number to the list , sum
+    # if no, the computer gets another card, if sum is <17, computer gets another card
 
-cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+# A starts to be 11 until player goes over 21
+    # sum > 21 Bust
+import random
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+player_random_numbers = random.sample(cards, 2)
+player_sum = sum(player_random_numbers)
 
-# start, y or n
-# your cards are [num1, numn2], sum 
-# computer's cards are [num1, num2], sum
+computer_random_numbers = random.sample(cards, 1)
+print(f"Your cards are {player_random_numbers} total is {player_sum}. Computer cards are: {computer_random_numbers}")
 
-# do you want anohter card? 
-  # if y, add num3, sum 
+while player_sum < 21:
+    player_choice = input("Do you want another card? ")
+    new_random_number = random.choice(cards)
+
+    if player_choice == 'y':
+        player_random_numbers.append(new_random_number)
+        print(f"Your new card is {new_random_number}, your have {player_random_numbers}, total is {player_sum} ")
+    else:
+        computer_random_numbers.append(new_random_number)
+

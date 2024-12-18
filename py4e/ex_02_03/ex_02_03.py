5830efa8d873916d4435eaa1518023872c3ab7ac
June Zhao <bz.bijun@gmail.com>
2024-12-18 15:51:22 -0800

    py4e initial lesson 1-3

---
+++ py4e/ex_02_03/ex_02_03.py
@@ -0,0 +1,5 @@
+xh = input('Enter Hours: ')
+xr = input('Enter Rate: ')
+
+xp = int(xh) * int(xr)
+x = int(98.6)
---
+++ py4e/ex_02_03/ex_03.py
@@ -0,0 +1,29 @@
+x = 5
+# if x == 5 :
+#    print("x is equal to 5")
+# if x > 4 : 
+#    print("x is greater than 4")
+# if x >= 5 :
+#    print("x is greater than or equal to 5")
+# if x < 6 :
+#    print("x is less than 6")
+# if x <=5 : 
+#    print("x is less than or equal to 5")
+# if x != 6: 
+#    print("x is not equal to 6")
+
+# print('Before 5')
+# if x == 5: 
+#   print('Is 5')
+#   print("Is still 5")
+#   print("Third 5")
+
+# print("Afterwards 5")
+# print("Before 6")
+
+# if x == 6:
+#   print('Is 6')
+#   print("Is still 6")
+#   print("Third 6")
+
+# print("Afterwards 6")
